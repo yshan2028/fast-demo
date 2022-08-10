75f38961bf0d87fe1c31bd52b5f746b0cdc077d0
nhnhwsnh <464138433@qq.com>
2022-08-10 11:21:25 +0800

    更新mysql的初始数据

--- deploy/mysql/03-role.sql
+++ deploy/mysql/03-role.sql
@@ -1,3 +1,3 @@
-UPDATE fa_demo.role SET create_time = '2022-08-06 09:09:12.152306', update_time = '2022-08-09 21:50:12.679888', role_name = '菜单管理员', order_no = 3, status = 1, remark = null WHERE id = 1;
-UPDATE fa_demo.role SET create_time = '2022-08-07 12:00:15.976658', update_time = '2022-08-09 21:50:08.498582', role_name = '角色管理员', order_no = 2, status = 1, remark = null WHERE id = 16;
-UPDATE fa_demo.role SET create_time = '2022-08-09 20:39:52.067369', update_time = '2022-08-09 21:50:16.404935', role_name = '账号管理员', order_no = 1, status = 1, remark = null WHERE id = 18;
+INSERT INTO fa_demo.role (id, create_time, update_time, role_name, order_no, status, remark) VALUES (1, '2022-08-06 09:09:12.152306', '2022-08-09 21:50:12.679888', '菜单管理员', 3, 1, null);
+INSERT INTO fa_demo.role (id, create_time, update_time, role_name, order_no, status, remark) VALUES (16, '2022-08-07 12:00:15.976658', '2022-08-09 21:50:08.498582', '角色管理员', 2, 1, null);
+INSERT INTO fa_demo.role (id, create_time, update_time, role_name, order_no, status, remark) VALUES (18, '2022-08-09 20:39:52.067369', '2022-08-09 21:50:16.404935', '账号管理员', 1, 1, null);
