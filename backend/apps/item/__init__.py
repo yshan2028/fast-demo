8a358d9fba1601952d03bdee98838163120ca21a
rongli <464138433@qq.com>
2022-09-21 17:36:46 +0800

    删除没用的apps目录

--- backend/apps/item/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python 
-# -*- coding:utf-8 -*-
-# Time:    2022-08-30 13:44
-# Author:  rongli
-# Email:   abc@xyz.com
-# File:    __init__.py.py
-# Project: fa-demo
-# IDE:     PyCharm
--- backend/apps/item/dependencies.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python 
-# -*- coding:utf-8 -*-
-# Time:    2022-08-30 13:44
-# Author:  rongli
-# Email:   abc@xyz.com
-# File:    dependencies.py
-# Project: fa-demo
-# IDE:     PyCharm
-
